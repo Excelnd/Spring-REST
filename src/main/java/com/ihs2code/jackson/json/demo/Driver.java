9eb520a4401eff10fca6c65e0caeba4ef15dfa07
Admin i <ihsanmpm@gmail.com>
2021-07-17 21:12:08 +0530

    init

---
+++ src/main/java/com/ihs2code/jackson/json/demo/Driver.java
@@ -0,0 +1,29 @@
+package com.ihs2code.jackson.json.demo;
+
+import java.io.File;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		
+		try {
+			// create object mapper
+			
+			// read JSON  file and map/convert to Java POJO: 
+			// data/sample-lite.json
+			
+			
+			// print first name and last name
+
+		}
+		catch (Exception exc) {
+			exc.printStackTrace();
+		}
+	}
+}
+
+
+
+
